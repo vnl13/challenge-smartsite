91efad4b610ed521cb141f574ef39f17f9b22eda
Victor Lima <victor-lima26@hotmail.com>
2022-01-30 02:50:41 -0300

    refactor: add all components to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from 'next';
 
+import { Container } from '../styles/home';
+import { Header } from '../components/Header';
+import { Form } from '../components/Form';
+import { Legend } from '../components/Legend';
+import { List } from '../components/List';
+import { Footer } from '../components/Footer';
+
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <h2>
+          REABERTURA <span>SMART FIT</span>
+        </h2>
+        <p>
+          O horário de funcionamento das nossas unidades está seguindo os
+          decretos de cada município. Por isso, confira aqui se a sua unidade
+          está aberta e as medidas de segurança que estamos seguindo.
+        </p>
+        <Form />
+        <Legend />
+        <List />
+      </Container>
+      <Footer />
+    </>
   );
 };
 
