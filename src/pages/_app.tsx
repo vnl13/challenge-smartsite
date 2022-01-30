4cc3b6b63d4bfd0edc2c69737620f07b4fbc9fa9
Victor Lima <victor-lima26@hotmail.com>
2022-01-30 02:50:05 -0300

    refactor: add global style and locations provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../styles/global';
+import { LocationsProvider } from '../contexts/Locations';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <LocationsProvider>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </LocationsProvider>
+  );
 }
 
 export default MyApp;
